ee1431664df93f6d30f34a7de45fd78c29807668
Manuel Ortega <manuel@enkidu.coop>
2016-03-15 09:43:53 +0100

    Agregando enlace al catálogo

--- js/sharings.js
+++ js/sharings.js
@@ -2,19 +2,28 @@
 var sharingsTranslations = {
     sv: {
         sharingsLongHeadline: "Share",
-        sharingsShortHeadline: "Share"
+        sharingsShortHeadline: "Share",
+        catalogLongHeadline: "Catalog",
+        catalogShortHeadline: "Catalog"
+
     },
     eo: {
         sharingsLongHeadline: "Kundividu objektojn kaj servojn en la reto",
-        sharingsShortHeadline: "Kundividi"
+        sharingsShortHeadline: "Kundividi",
+        catalogLongHeadline: "Vidi la katalogon de kundividitaj objektoj kaj servoj",
+        catalogShortHeadline: "Katalogo"
     },
     en: {
         sharingsLongHeadline: "Share objects and services in the network",
-        sharingsShortHeadline: "Share"
+        sharingsShortHeadline: "Share",
+        catalogLongHeadline: "Display the catalog of shared objects and services",
+        catalogShortHeadline: "Catalog"
     },
     es: {
         sharingsLongHeadline: "Comparte objetos y servicios en la red",
-        sharingsShortHeadline: "Compartir"
+        sharingsShortHeadline: "Compartir",
+        catalogLongHeadline: "Ver el catálogo de objetos y servicios compartidos",
+        catalogShortHeadline: "Catálogo"
     }
 };
 
@@ -29,6 +38,13 @@ $(document).bind('qvitterAfterLanguageIsSet', function() {
     $('#main-menu').append('\
         <a class="stream-selection" href="' + window.siteInstanceURL + 'main/sharings/new" data-tooltip="' + window.sL.sharingsLongHeadline + '">\
             ' + window.sL.sharingsShortHeadline + '\
+            <i class="chev-right"></i>\
+        </a>');
+
+    // add show catalog to menu-container in UI
+    $('#main-menu').append('\
+        <a class="stream-selection" href="' + window.siteInstanceURL + 'tag/sharings" data-tooltip="' + window.sL.catalogLongHeadline + '">\
+            ' + window.sL.catalogShortHeadline + '\
             <i class="chev-right"></i>\
         </a>');
 });
